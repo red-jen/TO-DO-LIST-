1651aefa87d419083423ccac7614e89cc5ca65de
red-jen <redaje102105@gmail.com>
2024-10-31 15:04:43 +0100

    1st interface done

--- brief1/to-do.js
+++ brief1/to-do.js
@@ -1,11 +1,27 @@
 const addTask = document.getElementById('addTask');
 const Multiple = document.getElementById('Multiple');
 const addTaskModal = document.getElementById('addTaskModal');
-let myH
+const MyHidde = document.getElementById('MyHidde')
+const Task = document.getElementById('Task')
 
+
+
+// add task function
 function faddTask() {
-  alert('It was clicked!');
-  addTaskModal.classList.add('show');
+  // alert('It was clicked!');
+  addTaskModal.classList.remove('hidden');
+
 
 }
 addTask.addEventListener('click' , faddTask);
+//copie and creat the tasks
+const clonedTask = Task.cloneNode(true); // 'true' means a deep copy, including child elements
+clonedTask.id = 'newElementId'; //Change the ID to avoid duplicates
+
+
+
+// annuler function 
+function fhiddetask() {
+  addTaskModal.classList.add('hidden');
+}
+MyHidde.addEventListener('click' ,fhiddetask )
